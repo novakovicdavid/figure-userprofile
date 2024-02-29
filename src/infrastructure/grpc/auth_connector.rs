7b23d5c27f58c174333dbd2043b3f6514cb900af
David Novakovic <83370778+novakovicdavid@users.noreply.github.com>
2024-02-29 02:06:24 +0100

    Add correlation id to auth connector

--- src/infrastructure/grpc/auth_connector.rs
+++ src/infrastructure/grpc/auth_connector.rs
@@ -1,20 +1,26 @@
+use tonic::{Request, Status};
+use tonic::service::Interceptor;
+
 pub use auth_connector::GrpcAuthConnector;
+use crate::infrastructure::http::middleware::correlation_id_layer::get_correlation_id;
 
 mod auth_connector {
     use std::str::FromStr;
 
     use async_trait::async_trait;
+    use tonic::codegen::InterceptedService;
     use tonic::transport::{Channel, Endpoint};
     use tower::ServiceBuilder;
 
     use auth_client::AuthClient;
 
     use crate::application::connectors::auth_connector::{AuthConnector, AuthConnectorError};
+    use crate::infrastructure::grpc::auth_connector::CorrelationIdInterceptor;
 
     tonic::include_proto!("auth");
 
     pub struct GrpcAuthConnector {
-        client: AuthClient<Channel>,
+        client: AuthClient<InterceptedService<Channel, CorrelationIdInterceptor>>,
     }
 
     impl GrpcAuthConnector {
@@ -26,8 +32,10 @@ mod auth_connector {
             let channel = ServiceBuilder::new()
                 .service(channel);
 
+            let client = AuthClient::with_interceptor(channel, CorrelationIdInterceptor);
+
             Ok(Self {
-                client: AuthClient::new(channel)
+                client
             })
         }
     }
@@ -47,5 +55,21 @@ mod auth_connector {
                 .map(|response| response.into_inner().session_token)
                 .map_err(|status| AuthConnectorError::UnexpectedError(anyhow::Error::msg(status)))
         }
+    }
+}
+
+#[derive(Clone)]
+pub struct CorrelationIdInterceptor;
+
+impl Interceptor for CorrelationIdInterceptor {
+    fn call(&mut self, mut request: Request<()>) -> Result<Request<()>, Status> {
+        let correlation_id = get_correlation_id()
+            .ok_or_else(|| Status::internal("Couldn't get the correlation id...?"))?;
+
+        request
+            .metadata_mut()
+            .insert("x-correlation-id", correlation_id.parse().unwrap());
+
+        Ok(request)
     }
 }
